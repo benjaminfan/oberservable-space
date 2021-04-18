957de5915eef6e1841bcb248844f3b983b0c647f
benjaminfan <ben.yan.fan@gmail.com>
2021-04-18 01:37:38 -0500

    Playing with data

--- service/agents.py
+++ service/agents.py
@@ -1,2 +1,20 @@
+import json
+import pandas as pd
+
+
 class Agents:
-    pass
+    REQUIRED_FIELDS = ('_id')
+    OPTIONAL_FIELDS = ('name', 'address', 'state', 'zipcode', 'tier', 'phone')
+
+    def __init__(self, filepath='./data/agents.json'):
+        reset_list(filepath)
+
+    def load_data(self, string: filepath) -> pd.DataFrame:
+        with open(filepath) as f:
+            data = json.load(f)
+            df = pd.DataFrame([agents], index=['_id'])
+
+    def __repr__(self):
+        return "{} {} {} {} {}".format(self.first_name, self.last_name, self.birth_date, self.sex, self.address)
+
+    def create_(self, agent):
